1c9afca7ebeba7ea7bddfb159ab2bb72c766d6b9
ValentinaTironi <valentina.tironi@anima.edu.uy>
2018-10-24 17:52:17 -0300

    connect with openweather api

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     var type_input = $('a.active').attr("type_input");
     show_description('a.active', type_input);
-
+    
     $('a.item').on('click', function active() {
+        var type_input = $(this).attr("type_input");
+        var option = $(this).html();
+        var coordinates_element = $('.input-lat').length;
+        var active_option = $('a.active').html();
+        if (!coordinates_element && active_option != "Geographic coordinates") {
+            $('div.input-fields').append('<div class="field"><label class="input-label label-lat"></label><input class="input-lat" type="number" name="input-lat" placeholder=""></div>');
+            $('input.inpu').addClass('input-lon');
+        } else if(option != "Geographic coordinates") {
+            $('.label-lat').remove();
+            $('.input-lat').remove();            
+        }
         $('a.active').removeClass('active');
         $(this).addClass('active');
-        var type_input = $(this).attr("type_input");
         show_description(this, type_input);
     });
 
@@ -13,11 +23,14 @@ $(document).ready(function() {
         var type = $(identificator).html();
         $('h4.title').html('Please, fill the input with the ' + type.toLowerCase());
         $('.input-label').html(type);
-        $('.input').attr("placeholder", type);
+        if (type == "Geographic coordinates") {
+            $('.input-lat').attr('placeholder', 'Latitude');
+            $('.input').attr('placeholder', 'Longitude');     
+            $('.input-label').html('Longitude');       
+            $('.label-lat').html("Latitude");
+        } else {
+            $('.input').attr("placeholder", type);
+        }
         $('.input').attr("type", type_input);
-    }
-
-    function create_input(){
-        $('form.form').append('<div class="field"><label class="input-label"></label><input class="input" type="text" name="first-name" placeholder=""></div>')
     }
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
+require('dotenv').config()
 var express = require('express');
 var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res, next) {
   res.render('index');
+  by_city_name('montevideo');
 });
+
+function by_city_name(city_name) {
+  city_name = capitalize_first_letter(city_name);
+  request(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${process.env.APPID}`, function (error, response, body) {
+    console.log(body);    
+  });
+}
+
+
+function capitalize_first_letter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 module.exports = router;
